6b205898507cdafccea9575b0029b526700d8e22
karan <karanbharti@gmail.com>
2025-04-01 17:37:47 +0530

    add accordian

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@
 import "./App.css";
 import { useRoutes } from "react-router-dom";
 import { Task1 } from "./components/task";
+import Accordian from "./components/accordian/Accordian";
 
 function App() {
-  const routes = useRoutes([{ path: "/", element: <Task1 /> }]);
+  const routes = useRoutes([{ path: "/", element: <Task1 /> },{ path: "/accordian", element: <Accordian /> }]);
   return routes;
 }
 
---
+++ src/components/accordian/Accordian.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./Accordian.css";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+
+const Accordian = () => {
+  const [arrow, setarrow] = useState(true);
+
+  const handleArrowClick = () => {
+    setarrow(!arrow);
+  };
+
+  return (
+    <div className={arrow ? "accordianMainContainer" : "hideContent"}>
+      <div
+        className={
+          arrow ? "accordianUpperSection" : "accordianUpperSection-show"
+        }
+      >
+        <p>Accordion 1</p>
+        {arrow ? (
+          <IoIosArrowUp
+            onClick={handleArrowClick}
+            cursor="pointer"
+            fontSize="25px"
+          />
+        ) : (
+          <IoIosArrowDown
+            onClick={handleArrowClick}
+            cursor="pointer"
+            fontSize="25px"
+          />
+        )}
+      </div>
+
+      <div className={arrow ? "accordianLowerSection" : "hideContent-close"}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+          error quae. Magni numquam cumque in exercitationem maxime ut culpa,
+          facilis voluptate corporis, inventore iure eveniet. Aut rerum
+          laudantium accusantium voluptates, doloribus totam adipisci quaerat
+          ipsum!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Accordian;
